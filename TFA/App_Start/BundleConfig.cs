763cc2be61b97fb5422fd44abe33043daaefd6cb
Jessi Sibayan <jessisibayan@gmail.com>
2017-09-25 14:46:34 +0800

    Change bootstrap theme
    Change background image

--- TFA/App_Start/BundleConfig.cs
+++ TFA/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TFA
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/Site.css"));
         }
     }
