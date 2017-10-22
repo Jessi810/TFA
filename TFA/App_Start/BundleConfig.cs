6c0169a51d78ee32bd11aa3c449e26f865ef850e
Jessi Sibayan <jessisibayan@gmail.com>
2017-10-22 19:39:42 +0800

    Added password strength meter

--- TFA/App_Start/BundleConfig.cs
+++ TFA/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace TFA
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/pwstrength-bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-darkly.css",
